d4468483adc55097ea6f88716ba5b698857b88dc
Enrique Montero <enrique.montero@newfold.com>
2026-05-30 16:09:22 -0800

    new vpc peering v15

--- environments/main.tf
+++ environments/main.tf
@@ -19,7 +19,7 @@ module "vpc_private" {
         },
         {
           range_name    = "services"
-          ip_cidr_range = "10.0.0.0/20"
+          ip_cidr_range = "10.5.0.0/20"
         }
       ]
     }
